a653967c87c1218da24898d5b9738c2fb4e833bf
Priyammondal <priyammondal096@gmail.com>
2026-01-11 23:30:52 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 },
+      pngquant: { quality: [0.65, 0.9], speed: 4 },
+      svgo: {
+        plugins: [
+          { name: "removeViewBox" },
+          { name: "removeEmptyAttrs", active: false },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
     },
+  },
+  build: {
+    target: "es2015",
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    sourcemap: false, // no sourcemap for production
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 500, // optional, increases warning threshold
+  },
+  css: {
+    devSourcemap: false,
   },
 });
